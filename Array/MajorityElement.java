7fa280645c293821079e455baea21b54edbbf862
Ayush Jain <ayushjain@Ayushs-MacBook-Pro.local>
2024-10-04 01:00:04 +0530

    Majority Elements updated

--- Array/MajorityElement.java
+++ Array/MajorityElement.java
@@ -42,17 +42,17 @@ public class MajorityElement {
         int cnt1 = 0, cnt2 = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == el1)
+            if (cnt1 == 0 && arr[i] != el2) {
+                el1 = arr[i];
+                cnt1 = 1;
+            } else if (cnt2 == 0 && arr[i] != el1) {
+                el2 = arr[i];
+                cnt2 = 1;
+            } else if (arr[i] == el1)
                 cnt1++;
             else if (arr[i] == el2)
                 cnt2++;
-            else if (cnt1 == 0) {
-                el1 = arr[i];
-                cnt1 = 1;
-            } else if (cnt2 == 0) {
-                el2 = arr[i];
-                cnt2 = 1;
-            } else {
+            else {
                 cnt1--;
                 cnt2--;
             }
